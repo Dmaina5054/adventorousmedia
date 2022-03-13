4a149b75467ef229f7913cad48e2b47984ed07da
Dmaina5054 <dmain5054@gmail.com>
2022-03-13 12:37:59 +0300

    added heroku

--- photography/urls.py
+++ photography/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('advtour.urls',namespace='advtour'))
+    path('', include('advtour.urls',namespace='advtour'))
 
 ]
